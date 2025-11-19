60ae8121e3c4dde781ae937dd3a3ebb0f69a3f33
alydhicks <alydhicks@gmail.com>
2025-11-18 21:27:58 -0600

    Add loading of negative protein sequences from CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ st.title('🎈 Protein-Protein Interaction App')
 
 st.write('This is a PPI machine learning app!')
 
+neg_df = pd.read_csv(https://raw.githubusercontent.com/alydhicks/Protein-Files/refs/heads/main/negative_protein_sequences.csv?token=GHSAT0AAAAAADPR4FV4757PBORWLFQWJ26I2I5G77Q)
+neg_df                     
+
+pos_df = pd.read_csv()
