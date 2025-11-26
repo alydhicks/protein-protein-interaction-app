bace2e6a59e34ea7afd1cb9116b09da549ac9118
alydhicks <alydhicks@gmail.com>
2025-11-26 11:58:23 -0600

    Update positive protein sequences CSV URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,5 @@ st.write('This is a PPI machine learning app!')
 neg_df = pd.read_csv('https://github.com/alydhicks/protein-protein-interaction-app/blob/master/negative_protein_sequences_250.csv')
 neg_df.head()
 
-pos_df = pd.read_csv('')
+pos_df = pd.read_csv('https://github.com/alydhicks/protein-protein-interaction-app/blob/master/positive_protein_sequence_250.csv')
+pos_df.head()
