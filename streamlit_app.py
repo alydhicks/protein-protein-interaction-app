5dc666340f32a98da4d9b980649369c8307f9cbc
alydhicks <alydhicks@gmail.com>
2025-11-26 12:23:10 -0600

    Update positive protein sequences data source

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ st.write('This is a PPI machine learning app!')
 neg_df = pd.read_csv('https://raw.githubusercontent.com/alydhicks/Protein-Files/refs/heads/main/negative_protein_sequences.csv')
 neg_df.head()
 
-pos_df = pd.read_csv('')
+pos_df = pd.read_csv('https://raw.githubusercontent.com/alydhicks/Protein-Files/refs/heads/main/positive_protein_sequences.csv')
 pos_df.head()
